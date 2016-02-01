4e8ed763fe4f9566ad6b6beb1f3eff04577dbef4
Maxime <MaximePawlakFr@users.noreply.github.com>
2016-02-01 12:51:30 +0100

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,29 @@
+<?php
+
+$nom = "mon petit nom";
+
+
+
+$age = 25;
+
+
+$majeurDepuis = $age - 18 ;
+
+// echo $majeurDepuis;
+
+$phrase = "Je suis majeur depuis " . $majeurDepuis . " ans!!!!!!!" ;
+
+echo '\n';
+echo $phrase;
+echo "\n";
+echo "coucou";
+echo '\n';
+echo "Les caractères d'échappement ne fonctionnent qu'avec les double-guillements.";
+echo 'bonjour \n 
+ça va asv ?';
+
+echo "bonjour \n
+ça va asv ?";
+
+
+
